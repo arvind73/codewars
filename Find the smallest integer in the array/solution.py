5ba9856658227109136ed2f78cfe068a3b0ac021
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2020-12-26 22:21:13 +0530

    Update solution.py

--- Find the smallest integer in the array/solution.py
+++ Find the smallest integer in the array/solution.py
@@ -8,6 +8,7 @@ Given [34, 15, 88, 2] your solution will return 2
 Given [34, -345, -1, 100] your solution will return -345
 You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
+Solution:
 def find_smallest_int(arr):
     minelement = arr[0]
     for i in range(1,len(arr)):
